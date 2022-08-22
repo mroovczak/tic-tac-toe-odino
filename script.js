207d4df1bc3019a82d6143bb3613a62b81b95d7e
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-08-22 14:14:06 +0200

    add todo

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-const GameBoard = (() =>{
+const gameBoard = (() =>{
     let gameboard = [
         [,,],
         [,,],
         [,,]
-    ] //create empty 3x3 array of board
-}
+    ]; //create empty 3x3 array of
 
+    return {
+        gameboard,
+    }
+})();
 
-)
+const displayController = (() => {
+    const board = document.querySelector('#board');
+    const gameboard = gameBoard.gameboard;
+    const drawFields = (gameboard) => {
+        gameboard.array.forEach((field, index) => {
+           let element = document.createElement("div").classList.add(index);
+           board.appendChild(element);
+        });
+    }
+    // drawFields(gameboard);
+
+    return {
+        board,
+    }
+})();
+console.log(gameBoard.gameboard);
+gameBoard.gameboard.forEach((field, index) => {
+    let element = document.createElement("div");
+    element.classList.add(index);
+    console.log(element);
+    displayController.board.appendChild(element);
+    console.log("s");
+ });
