a400a79bd36878cac9848c41360178a5dbbc77d3
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-08-26 13:34:32 +0200

    add player factory and create two example players

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//gameBoard Modulke
 const gameBoard = (() =>{
     let gameboard = [
         [null,null,null],
@@ -10,23 +11,59 @@ const gameBoard = (() =>{
     }
 })();
 
+//Player factory
+const Player = (name,sign) => {
+    const getName = () => name;
+    const getSign = () => sign;
+    return {
+        getName,
+        getSign,
+    }
+};
+
+// displayController module
 const displayController = (() => {
     const board = document.querySelector('#board');
-    const gameboard = gameBoard.gameboard;
-    const drawFields = (gameboard) => {
+    let gameboard=[
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+    ]; //later filled with field elements
+    const drawFields = () => {
         gameBoard.gameboard.forEach((row, indexX) => {
             row.forEach((field,indexY)=> {
                 let element = document.createElement("div");
                 element.setAttribute("class","field");
                 element.setAttribute("data-x",indexX);
                 element.setAttribute("data-y",indexY);
-                this.board.appendChild(element);
+                element.setAttribute("data-sign","");
+                board.appendChild(element);
+                gameboard[indexX][indexY]=element; //add elements to array instead of SelectQuerry them later
+                
             });
          });
     }
-    drawFields(gameboard);
+    const addListeners = () => {
+        gameboard.forEach((row,indexX)=> {
+            row.forEach((element,indexY) => {
+                element.addEventListener("click", (e)=>{
+                    e.target.setAttribute("data-sign",player1.getSign());
+                    gameBoard.gameboard[indexX][indexY]=player1.getSign();
+                    console.table(gameBoard.gameboard);
+                });
+            });
+        });
+    }
+    //drawFields(gameboard);
 
     return {
         board,
+        drawFields,
+        addListeners,
     }
 })();
+displayController.drawFields();
+displayController.addListeners();
+const player1 = Player("Antek","x");
+const player2 = Player("Mietek","o");
+console.log(player1.getName());
