77fc92af71cae46b2c108d23838d7b3c180b4765
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-08-26 12:39:43 +0200

    add styling to board and move it drawing it to the module

--- script.js
+++ script.js
@@ -14,24 +14,19 @@ const displayController = (() => {
     const board = document.querySelector('#board');
     const gameboard = gameBoard.gameboard;
     const drawFields = (gameboard) => {
-        gameboard.array.forEach((field, index) => {
-           let element = document.createElement("div").classList.add(index);
-           board.appendChild(element);
-        });
+        gameBoard.gameboard.forEach((row, indexX) => {
+            row.forEach((field,indexY)=> {
+                let element = document.createElement("div");
+                element.setAttribute("class","field");
+                element.setAttribute("data-x",indexX);
+                element.setAttribute("data-y",indexY);
+                this.board.appendChild(element);
+            });
+         });
     }
-    // drawFields(gameboard);
+    drawFields(gameboard);
 
     return {
         board,
     }
 })();
-console.log(gameBoard.gameboard);
-gameBoard.gameboard.forEach((row, indexX) => {
-    row.forEach((field,indexY)=> {
-        let element = document.createElement("div");
-        element.setAttribute("class","field");
-        element.setAttribute("data-x",indexX);
-        element.setAttribute("data-y",indexY);
-        displayController.board.appendChild(element);
-    });
- });
